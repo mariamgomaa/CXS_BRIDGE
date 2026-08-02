099aead360daa6fa6550f79c42965facb58dff65
Mariam <mariam.mostafa04@eng-st.cu.edu.eg>
2026-08-02 04:34:26 +0300

    load the encryption and parity unit

---
+++ encryption_unit.sv
@@ -0,0 +1,86 @@
+module encryption_unit  #(
+    parameter int DATA_WIDTH =8 ,
+    localparam int OUTPUT_DATA_WIDTH = 2 * DATA_WIDTH
+) (
+    input  logic [DATA_WIDTH-1 : 0] i_encryption_unit_payload_data,
+    input  logic                    i_encryption_unit_payload_valid,
+    input  logic                    i_encryption_unit_encryption_mode,   // 0: Even, 1: Odd
+
+    output logic [OUTPUT_DATA_WIDTH-1 : 0] o_encryption_unit_encrypted_data,
+    output logic                           o_encryption_unit_encrypted_valid
+);
+    
+    
+    integer i;
+    integer ones_cnt;
+    integer zeros_cnt;
+
+    always_comb begin
+
+        //default values 
+        o_encryption_unit_encrypted_data  = 16'd0;
+        o_encryption_unit_encrypted_valid =  i_encryption_unit_payload_valid;
+
+        // Count number of ones
+        ones_cnt = 0;
+        zeros_cnt = 0;
+        for (i = 0; i < 8; i = i + 1)
+        begin
+        if (i_encryption_unit_payload_data[i] ==1'b1)
+            ones_cnt = ones_cnt + i_encryption_unit_payload_data[i];
+        else 
+            zeros_cnt = zeros_cnt + i_encryption_unit_payload_data[i];
+        end 
+
+
+        if ( i_encryption_unit_payload_valid) begin
+            o_encryption_unit_encrypted_valid = 1'b1;
+            //----------------------------------------
+            // Special Case
+            //----------------------------------------
+            if ((ones_cnt == zeros_cnt) ||
+                ((ones_cnt % 2 == 0) && (zeros_cnt % 2 == 0))) begin
+
+                for (i = 0; i < 8; i = i + 1) begin
+                    if (i_encryption_unit_payload_data[i] == 1'b0)
+                        o_encryption_unit_encrypted_data[2*i +: 2] = 2'b01;
+                    else
+                        o_encryption_unit_encrypted_data[2*i +: 2] = 2'b10;
+                end
+
+            end
+
+            //----------------------------------------
+            // Even Expansion Mode
+            //----------------------------------------
+            else if (i_encryption_unit_encryption_mode == 1'b0) begin
+
+                for (i = 0; i < 8; i = i + 1) begin
+                    if (i_encryption_unit_payload_data[i] == 1'b0)
+                        o_encryption_unit_encrypted_data[2*i +: 2] = 2'b00;
+                    else
+                        o_encryption_unit_encrypted_data[2*i +: 2] = 2'b10;
+                end
+
+            end
+
+            //----------------------------------------
+            // Odd Expansion Mode
+            //----------------------------------------
+            else begin
+
+                for (i = 0; i < 8; i = i + 1) begin
+                    if (i_encryption_unit_payload_data[i] == 1'b0)
+                        o_encryption_unit_encrypted_data[2*i +: 2] = 2'b01;
+                    else
+                        o_encryption_unit_encrypted_data[2*i +: 2] = 2'b11;
+                end
+
+            end
+        end
+    end
+    
+endmodule
+
+
+
---
+++ parity_unit.sv
@@ -0,0 +1,43 @@
+module parity_unit #(parameter int DATA_W = 16) (
+
+    input  logic [DATA_W-1:0] i_parity_unit_encrypted_data,
+    input  logic        i_parity_unit_encrypted_valid,
+    input  logic        i_parity_unit_parity_mode,     // 0: Even, 1: Odd
+
+    output logic [DATA_W:0] o_parity_unit_data_with_parity,
+    output logic        o_parity_unit_parity_done
+
+);
+
+    logic parity_bit;
+
+    always_comb begin
+
+        // Default outputs
+        o_parity_unit_data_with_parity = '0;
+        o_parity_unit_parity_done      = 1'b0;
+        parity_bit                     = 1'b0;
+
+        if (i_parity_unit_encrypted_valid) begin
+
+            // XOR reduction of all data bits odd detection 
+            parity_bit = ^i_parity_unit_encrypted_data; // if number odd --> 1 even -->0 
+
+            // Even parity
+            if (i_parity_unit_parity_mode == 1'b0)
+            // Append parity as MSB
+            o_parity_unit_data_with_parity = {parity_bit,
+                                              i_parity_unit_encrypted_data};
+            // Odd parity
+            else
+            // Append parity as MSB
+            o_parity_unit_data_with_parity = {~parity_bit,
+                                              i_parity_unit_encrypted_data};
+
+            o_parity_unit_parity_done = 1'b1;
+
+        end
+
+    end
+
+endmodule
