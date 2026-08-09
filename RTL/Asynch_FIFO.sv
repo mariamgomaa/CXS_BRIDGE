8ad70f39264233c2deb30a62227ad476bc01c693
Mariam <mariam.mostafa04@eng-st.cu.edu.eg>
2026-08-09 14:57:52 +0300

    modify the asynchronous fifo to send buf releas

--- RTL/Asynch_FIFO.sv
+++ RTL/Asynch_FIFO.sv
@@ -1,18 +1,19 @@
 module async_fifo #(parameter DEPTH = 16, WIDTH = 39) ( // DEPTH FOR THIS VERSION SHOULD ALWAYS BE A POWER OF 2
-    input logic wr_clk,
-    input logic rd_clk,
+    input logic i_Asynch_FIFO_wr_clk,
+    input logic i_Asynch_FIFO_rd_clk,
 
-    input logic rd_rstn,
-    input logic wr_rstn,
+    input logic i_Asynch_FIFO_rd_rstn,
+    input logic i_Asynch_FIFO_wr_rstn,
 
-    input logic [WIDTH-1:0] data_in,
-    output logic [WIDTH-1:0] data_out,
+    input logic [WIDTH-1:0] i_Asynch_FIFO_data_in,
+    output logic [WIDTH-1:0] o_Asynch_FIFO_data_out,
 
-    input logic rd_en,
-    input logic wr_en,
+    input logic i_Asynch_FIFO_rd_en,
+    input logic i_Asynch_FIFO_wr_en,
 
-    output logic full,
-    output logic empty
+    output logic o_Asynch_FIFO_full,
+    output logic o_Asynch_FIFO_empty,
+    output logic o_Asynch_FIFO_buf_release
     
 );
 
@@ -25,27 +26,36 @@ logic [N:0]  rd_ptr_gray, wr_ptr_gray; // Read and Write Pointers (gray)
 logic [N:0]  rd_ptr_gray_FF1, wr_ptr_gray_FF1; 
 logic [N:0]  rd_ptr_gray_FF2, wr_ptr_gray_FF2; 
 
+//extra signal to represent the buf release for credit generator update 
+
+logic rd_en_gray;
+logic rd_en_gray_FF1,rd_en_gray_FF2 ;
+assign rd_en_gray = i_Asynch_FIFO_rd_en ^ (i_Asynch_FIFO_rd_en >> 1); // Binary to Gray code read en
+assign o_Asynch_FIFO_buf_release = rd_en_gray_FF2;
+
+////////////////////////////////////////////////////////////////////////
+
 assign rd_ptr_gray = rd_ptr ^ (rd_ptr >> 1); // Binary to Gray code read ptr
 
 assign wr_ptr_gray = wr_ptr ^ (wr_ptr >> 1); // Binary to Gray code write ptr
 
-assign full = (wr_ptr_gray[N-2:0]==rd_ptr_gray_FF2[N-2:0]) && (wr_ptr_gray[N :N-1] == ~rd_ptr_gray_FF2[N : N-1]);
+assign o_Asynch_FIFO_full = (wr_ptr_gray[N-2:0]==rd_ptr_gray_FF2[N-2:0]) && (wr_ptr_gray[N :N-1] == ~rd_ptr_gray_FF2[N : N-1]);
 
-assign empty = (wr_ptr_gray_FF2 == rd_ptr_gray);
+assign o_Asynch_FIFO_empty = (wr_ptr_gray_FF2 == rd_ptr_gray);
 
 logic [WIDTH-1:0] FIFO [0:DEPTH-1];
 
 
 // WRITE DOMAIN
 
-always_ff @(posedge wr_clk or negedge wr_rstn) begin
+always_ff @(posedge i_Asynch_FIFO_wr_clk or negedge i_Asynch_FIFO_wr_rstn) begin
 
-    if (!wr_rstn)
+    if (!i_Asynch_FIFO_wr_rstn)
         wr_ptr <= 0;
 
-    else if (!full && wr_en) begin
+    else if (!o_Asynch_FIFO_full && i_Asynch_FIFO_wr_en) begin
 
-        FIFO[wr_ptr[N-1:0]] <= data_in;
+        FIFO[wr_ptr[N-1:0]] <= i_Asynch_FIFO_data_in;
         wr_ptr <= wr_ptr + 1;
         
     end
@@ -55,25 +65,24 @@ end
 
 //READ DOMAIN
 
-always_ff @(posedge rd_clk or negedge rd_rstn) begin
+always_ff @(posedge i_Asynch_FIFO_rd_clk or negedge i_Asynch_FIFO_rd_rstn) begin
 
-    if (!rd_rstn)
+    if (!i_Asynch_FIFO_rd_rstn)
         rd_ptr <= 0;
 
-    else if (!empty && rd_en) begin
+    else if (!o_Asynch_FIFO_empty && i_Asynch_FIFO_rd_en) begin
 
-        data_out <= FIFO[rd_ptr[N-1:0]];
-        rd_ptr <= rd_ptr + 1;
-        
+        o_Asynch_FIFO_data_out <= FIFO[rd_ptr[N-1:0]];
+        rd_ptr <= rd_ptr + 1;     
     end
 
 end
 
 // 2-FF WRITE PTR
 
-always_ff @(posedge rd_clk or negedge rd_rstn) begin
+always_ff @(posedge i_Asynch_FIFO_rd_clk or negedge i_Asynch_FIFO_rd_rstn) begin
 
-    if (!rd_rstn) begin
+    if (!i_Asynch_FIFO_rd_rstn) begin
 
         wr_ptr_gray_FF1 <= 0;
         wr_ptr_gray_FF2 <= 0;
@@ -89,9 +98,9 @@ end
 
 // 2-FF READ PTR
 
-always_ff @(posedge wr_clk or negedge wr_rstn) begin
+always_ff @(posedge i_Asynch_FIFO_wr_clk or negedge i_Asynch_FIFO_wr_rstn) begin
 
-    if (!wr_rstn) begin
+    if (!i_Asynch_FIFO_wr_rstn) begin
 
         rd_ptr_gray_FF1 <= 0;
         rd_ptr_gray_FF2 <= 0;
@@ -101,6 +110,23 @@ always_ff @(posedge wr_clk or negedge wr_rstn) begin
 
         rd_ptr_gray_FF1 <= rd_ptr_gray;
         rd_ptr_gray_FF2 <= rd_ptr_gray_FF1;
+    end
+  
+end
+
+// 2-FF READ en for o_Asynch_FIFO_buf_release logic 
+always_ff @(posedge i_Asynch_FIFO_wr_clk or negedge i_Asynch_FIFO_wr_rstn) begin
+
+    if (!i_Asynch_FIFO_wr_rstn) begin
+
+        rd_en_gray_FF1 <= 0;
+        rd_en_gray_FF2 <= 0;
+    end
+    
+    else begin
+
+        rd_en_gray_FF1 <= rd_en_gray;
+        rd_en_gray_FF2 <= rd_en_gray_FF1;
     end
   
 end
